2ca4b4995163c841f240cc8d56a7b85127c4ada5
Pires <gabriela.pires@capgemini.com>
2023-01-20 10:28:08 -0300

    primeiro commit

---
+++ src/test/java/steps/DescontosStep.java
@@ -0,0 +1,37 @@
+package steps;
+
+import cucumber.api.java.es.Dado;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import pages.HomePage;
+import runner.RunCucumberTest;
+
+public class DescontosStep extends RunCucumberTest {
+
+    HomePage homePage = new HomePage(driver);
+
+    @Dado("^que eu estou no site da qazando")
+    public void acessar_site_qazando(){
+        homePage.acessarAplicacao();
+    }
+
+    @Quando("^eu preencho meu email$")
+    public void eu_preencho_meu_email() throws InterruptedException {
+        homePage.scrollDown();
+        homePage.preencheEmail();
+    }
+
+    @E("^clico em ganhar cupom$")
+    public void clico_em_ganhar_cupom(){
+        homePage.clickGanharDesconto();
+    }
+    @Entao("^eu vejo o código de desconto$")
+    public void eu_vejo_o_código_de_desconto(){
+        homePage.verificarCupomDesconto();
+
+    }
+}
